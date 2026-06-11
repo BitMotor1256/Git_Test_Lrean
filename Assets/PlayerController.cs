6c49da9180fa008ff66014b9e1afd9a0d8c1de9c
BitMotor <bitmotor@example.com>
2026-06-11 17:49:49 +0800

    Revert "fix: 临时调整移速"
    This reverts commit e1890836de63356849a92644250b6b84a37a6924.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1000f;
+    public float moveSpeed = 10f;
     public float jumpForce = 10f;
     private bool isGrounded = true;
 
