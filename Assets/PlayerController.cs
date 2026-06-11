490c2be90b0950a9698549b44b76ce10018e8c3a
BitMotor <bitmotor@example.com>
2026-06-11 20:23:12 +0800

    fix: 错误改动

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,15 +6,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     private bool isGrounded = true;
 
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    vo        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
